79b15917a4222a59d62cb4dbf595adc45e7dc8c5
Romte1 <ivan_ni224@hotmail.com>
2024-01-23 18:22:47 -0300

    Added a five-round function for the game, which is going to tell each round winner and final stats

--- javascript.js
+++ javascript.js
@@ -1,20 +1,34 @@
+
 function getComputerChoice(){
     let choice = Math.floor(Math.random()*100)+1;
     let choiceName = "";
-    (choice<=33) ? choiceName='Rock' : (choice>33 && choice<=66) ? choiceName='Paper' : (choice>66 && choice<=99) ? choiceName = 'Scissors': choiceName = "";
+    (choice<=33) ? choiceName='rock' : (choice>33 && choice<=66) ? choiceName='paper' : (choice>66 && choice<=99) ? choiceName = 'scissors': choiceName = "";
     return choiceName;
 }
 
-function playerSelection (selection){
-
+function playerSelection (){
+let selection = prompt("Hello, choose between Rock, Paper or Scissors to play!");
 let lowCaseSelection = selection.toLowerCase();
 
 return lowCaseSelection;
 
 }
 
-let selection = prompt("Hello, choose between Rock, Paper or Scissors to play!");
+function game() {
+let win = 0;
+let draw = 0;
+let lose = 0;
+    for (i = 0; i<5;i++){
+        playRound(playerSelection(), getComputerChoice())
+    
+    }    
+function playRound(playerSelection, computerSelection) {
+    (playerSelection === computerSelection) ? (draw++, console.log("It's a TIE!")) : (playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'rock') ? (lose++, console.log(`You lost, ${computerSelection} beats ${playerSelection}!`)) : (win++, console.log(`You win, ${playerSelection} beats ${computerSelection}!`)); 
 
-playerSelection(selection);
 
-getComputerChoice();
+
+  }
+}
+
+
+game();
